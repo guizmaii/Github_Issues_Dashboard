3448cb34d891e701b523c2d96ceae5426ce4e593
Jules Ivanic <jules.ivanic@gmail.com>
2014-05-24 18:26:07 +0200

    La sauvegarde dans Redis semble fonctionner. A tester plus en profondeur !

--- app/actors/RedisActor.scala
+++ app/actors/RedisActor.scala
@@ -5,7 +5,7 @@ import com.redis._
 import play.api.libs.json.JsValue
 import play.api.Logger
 
-case class RedisRepository(repoName: String, repoOwner: String, issues: List[JsValue])
+case class RedisRepository(name: String, owner: String, issues: List[JsValue])
 
 object RedisActor {
 
@@ -14,18 +14,31 @@ object RedisActor {
   val host: String = Play.current.configuration.getString("redis.host").get
   val port: Int = Integer.parseInt(Play.current.configuration.getString("redis.port").get)
 
+  val clients = new RedisClientPool(host, port)
+
 }
 
 class RedisActor extends Actor {
 
-  val client = new RedisClient(RedisActor.host, RedisActor.port)
-
   override def receive: Receive = {
 
+    // TODO : Coder & Tester la récupération des données
     case repo: RedisRepository =>
-      // TODO : Coder l'enregistrement des données
+      Logger.debug(s"RedisActor | Repo reçu pour sauvegarde : ${repo.owner}/${repo.name}")
+      
+      RedisActor.clients.withClient {
+        client => {
+          val key = s"data:${repo.owner}:${repo.name}"
 
-    case error =>
-      Logger.error("ERREUR : " + error)
+          client.del(key)
+
+          repo.issues.map(client.rpush(key, _))
+          Logger.info(s"RedisActor | N° of key inserted in key ($key) : ${client.llen(key)}")
+        }
+      }
+
+    case error: Exception =>
+      Logger.error(s"RedisActor | ERROR : ${error.getMessage}")
   }
+
 }
