cee9f9f43d33820c75f70a624b9c102b67785660
Jules Ivanic <jules.ivanic@gmail.com>
2014-05-25 16:52:02 +0200

    Ajout d'un TODO pour la partie web de l'application

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,8 @@ import play.api.mvc._
 
 object Application extends Controller {
 
+  //  TODO : Tester à l'ajout du dépot dans le system si celui-ci gére des issues
+
   implicit val context = scala.concurrent.ExecutionContext.Implicits.global
 
   def index = Action {
