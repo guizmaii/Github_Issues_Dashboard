8916d5a92a2a1481e5b4c1e10acb0b8e6fbc861a
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-06 01:28:57 +0200

    Suppression d'une case class devenu inutile

--- app/actors/compute/ComputedRepositoryData.scala
+++
@@ -1,6 +0,0 @@
-package actors.compute
-
-import domain.GraphType
-import play.api.libs.json.JsValue
-
-case class ComputedRepositoryData(graph: GraphType, repoName: String, repoOwner: String, computedData: List[JsValue])
