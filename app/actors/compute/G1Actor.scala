af2d8008fec100622b01d374e99230297d9ae1fa
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-08 21:49:34 +0200

    L'algo de calcule du G1 est OK et le temps d'execution (22 secondes pour environ ~15500 issues) est bonne.

--- app/actors/compute/G1Actor.scala
+++ app/actors/compute/G1Actor.scala
@@ -1,37 +1,65 @@
 package actors.compute
 
-import akka.actor.{PoisonPill, Actor}
-import actors.{RepositoryData, GithubRepository, Redisable}
+import akka.actor.{Props, PoisonPill, Actor}
+import actors.{GithubRepository, Redisable}
 import play.api.Logger
 import org.joda.time.DateTime
 import domain.{G1, GraphType}
 
 import play.api.libs.json._
-
-import play.api.libs.json.Reads._ // Custom validation helpers
+import scala.collection.mutable.ListBuffer
+import actors.RepositoryData
+import play.api.libs.concurrent.Akka
 
 case class G1ComputedData(repo: GithubRepository, computedData: java.util.TreeMap[String, Int], graphType: GraphType = G1)
 
+private case class G1Data(issuesChunk: List[JsObject], issues: List[JsObject])
+
+object G1Actor {
+  val CHUNK_SIZE = 1000
+}
+
 class G1Actor extends Actor with Redisable {
 
+  import play.api.Play.current
+
   val graphPoints = new java.util.TreeMap[String, Int]()
+  var repo: GithubRepository = null
+
+  var begin = 0L
+  var end = 0L
+
+  var workers = 0
 
   override def receive: Receive = {
 
     case data: RepositoryData =>
-      val before = System.currentTimeMillis()
-      data.issues map {
-        issue =>
-          val createdDate = (issue \ "created_at").as[String]
-          val parsedCreatedDate = DateTime.parse(createdDate)
-          graphPoints.put(createdDate, data.issues.count(isOpenAtThisDate(_, parsedCreatedDate)))
+      begin = System.currentTimeMillis()
+
+      repo = data.repo
+
+      val groupedIssuesIterator = data.issues.grouped(G1Actor.CHUNK_SIZE).toList
+      workers = groupedIssuesIterator.length
+      groupedIssuesIterator map ( Akka.system.actorOf(Props[G1Actor]) ! G1Data(_,  data.issues) )
+
+    case data: G1Data =>
+      val lighterList = getLighterList(data.issuesChunk)
+      lighterList map {
+        tuple =>
+          val parsedCreatedDate = DateTime.parse(tuple._1)
+          graphPoints.put(tuple._1, lighterList.count(isOpenAtThisDate(_, parsedCreatedDate)))
       }
-      val after = System.currentTimeMillis()
+      sender ! graphPoints
 
-      Logger.debug("time : " + ((after - before) / 1000) + " secondes")
+    case graphPointsChunk: java.util.TreeMap[String, Int] =>
+      this.graphPoints.putAll(graphPointsChunk)
+      workers -= 1
+      if (workers == 0) {
+        end = System.currentTimeMillis()
+        Logger.debug("TEMPS PRIS : " + ((end - begin) / 1000) + " secondes")
 
-      redisActor ! G1ComputedData(data.repo, graphPoints)
-      self ! PoisonPill
+        redisActor ! G1ComputedData(repo, graphPoints)
+      }
 
     case error: Exception =>
       Logger.error(s"${this.getClass} | ERROR : ${error.getMessage}")
@@ -40,28 +68,38 @@ class G1Actor extends Actor with Redisable {
       throw error
   }
 
-  private def isCreatedBeforeOrInSameTime(issue: JsObject, creationDate: DateTime): Boolean = {
-    val createdAt = DateTime.parse((issue \ "created_at").asInstanceOf[JsString].value)
+  private def getLighterList(issues: List[JsObject]): ListBuffer[(String, String)] = {
+    val lightIssueList = new ListBuffer[(String, String)]()
+    issues map {
+      issue =>
+        val created_at = (issue \ "created_at").asInstanceOf[JsString].value
+        val closed_at = issue \ "closed_at" match {
+          case json: JsString => json.value
+          case JsNull => null
+        }
+        lightIssueList += (created_at -> closed_at)
+    }
+    lightIssueList
+  }
+
+  private def isCreatedBeforeOrInSameTime(created_at: String, creationDate: DateTime): Boolean = {
+    val createdAt = DateTime.parse(created_at)
     createdAt.isBefore(creationDate) || createdAt.isEqual(creationDate)
   }
 
-  private def isClosedAfter(issue: JsObject, creationDate: DateTime): Boolean = {
-    issue \ "closed_at" match {
-      case closedDate: JsString =>
-        DateTime.parse(closedDate.value).isAfter(creationDate)
-      case JsNull =>
+  private def isClosedAfter(closed_at: String, creationDate: DateTime): Boolean = {
+    closed_at match {
+      case value: String =>
+        DateTime.parse(value).isAfter(creationDate)
+      case null =>
         // Si l'issue n'est pas closed alors
         // elle sera forcément fermé après la "creationDate"
         true
-      case otherType =>
-        Logger.error(s"${this.getClass} | ERROR : 'closed_at' received : $otherType")
-        false
     }
   }
 
-  // TODO : Validate
-  private def isOpenAtThisDate(issue: JsObject, creationDate: DateTime): Boolean = {
-    isCreatedBeforeOrInSameTime(issue, creationDate) && isClosedAfter(issue, creationDate)
+  private def isOpenAtThisDate(tuple: (String, String), creationDate: DateTime): Boolean = {
+    isCreatedBeforeOrInSameTime(tuple._1, creationDate) && isClosedAfter(tuple._2, creationDate)
   }
 
 }
