d223a53f746659dbfa751957c1272457fab7fb9a
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-14 21:28:00 +0200

    Optimisation autmatique des imports

--- app/actors/RedisActor.scala
+++ app/actors/RedisActor.scala
@@ -1,15 +1,14 @@
 package actors
 
+import actors.compute.G1.G1ComputedData
 import akka.actor.Actor
 import play.api.Logger
-import actors.compute.G1.G1ComputedData
 import traits.SyncRedisable
 
 class RedisActor extends Actor with SyncRedisable {
 
   override def receive: Receive = {
 
-    // TODO : Coder & Tester la récupération des données
     case g1Data: G1ComputedData =>
       val key = getRedisKey(g1Data.repo.owner, g1Data.repo.name, g1Data.graphType)
 
