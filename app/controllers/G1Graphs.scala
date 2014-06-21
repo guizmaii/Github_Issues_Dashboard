63bca0d2103fea1464ee8024f19c4eaba5ee874f
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-21 19:35:13 +0200

    Implémentation de la récupération de toutes les données coté Front

--- app/controllers/G1Graphs.scala
+++ app/controllers/G1Graphs.scala
@@ -1,6 +1,8 @@
 package controllers
 
 import domain.G1Type
+import models.{GithubRepository, GithubRepositoryDAO}
+import play.api.db.slick._
 import play.api.libs.json._
 import play.api.mvc._
 import spray.json._
@@ -17,23 +19,26 @@ object G1JsonProtocol extends DefaultJsonProtocol {
 object G1Graphs extends Controller with SyncRedisClient {
 
   import com.redis.serialization.Parse.Implicits._
+  import controllers.G1JsonProtocol._
 
-  def getAll = Action {
-    val data = redisPool.withClient {
-      client =>
-        client.hgetall[Long, Int](getRedisKey("scala", "scala", G1Type))
-    }
-
-    val sortedAndFormatedData = MapToArrayOfArrayOfLong(sortData(data.get))
-
-    import controllers.G1JsonProtocol._
-
-    // TODO : Find a way to not parse twice to JSON.
-    Ok(Json.parse(List(G1Json("Github_Issues_Dashboard", sortedAndFormatedData)).toJson.compactPrint))
+  def getAll = DBAction {
+    implicit rs =>
+      val data = GithubRepositoryDAO.getAll map {
+        implicit repo: GithubRepository =>
+          val data = redisPool.withClient {
+            client =>
+              client.hgetall[Long, Int](getRedisKey(repo.owner, repo.name, G1Type))
+          }
+          G1Json(repo.name, MapToArrayOfArrayOfLong(sortData(data.get)))
+      }
+      // TODO : Find a way to not parse twice to JSON.
+      Ok(Json.parse(data.toJson.compactPrint))
   }
 
+
+
   private def sortData(data: Map[Long, Int]): TreeMap[Long, Int] = {
-    TreeMap[Long, Int]().++( for(t <- data) yield t._1 -> t._2 )
+    TreeMap[Long, Int]() ++ ( for(t <- data) yield t._1 -> t._2 )
   }
 
   private def MapToArrayOfArrayOfLong(data: Map[Long, Int]): Array[Array[Long]] = {
