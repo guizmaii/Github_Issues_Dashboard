6f22fe96c24698f344ed36fe5c5a71f7c2d623f4
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-24 01:08:28 +0200

    MAJ launch.sh

--- launch.sh
+++ launch.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-cd ..
+rm -rf github_issues_dashboard-1.0-SNAPSHOT/
 
-rm -rf Github_Issues_Dashboard/github_issues_dashboard-1.0-SNAPSHOT/
+cd ..
 
 Github_Issues_Dashboard/activator play-update-secret
 Github_Issues_Dashboard/activator clean universal:package-zip-tarball
 tar zxvf Github_Issues_Dashboard/target/universal/*.tgz
 
-cd ..
-
-./github_issues_dashboard-1.0-SNAPSHOT/bin/github_issues_dashboard -J-javaagent:newrelic/newrelic.jar
+./Github_Issues_Dashboard/github_issues_dashboard-1.0-SNAPSHOT/bin/github_issues_dashboard -J-javaagent:newrelic/newrelic.jar
