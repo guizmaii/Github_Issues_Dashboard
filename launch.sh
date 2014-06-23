b84ab793e16698b5b62cc73d934286ccdb9715ed
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-24 01:55:25 +0200

    Ajout de la gestion de Redis en prod (fichier de conf + ajouts au launcher)

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cat /var/run/redis.pid | kill
+redis-server conf/redis/redis.prod.conf
+
 rm -rf github_issues_dashboard-1.0-SNAPSHOT/
 
 ./activator play-update-secret
@@ -8,4 +11,4 @@ tar zxvf target/universal/*.tgz
 
 cd github_issues_dashboard-1.0-SNAPSHOT/
 
-./bin/github_issues_dashboard -mem 8192 -J-server -J-javaagent:/home/Jules/newrelic/newrelic.jar -DapplyEvolutions.default=true -Dhttp.port=80
+sudo ./bin/github_issues_dashboard -mem 8192 -J-server -J-javaagent:/home/Jules/newrelic/newrelic.jar -DapplyEvolutions.default=true -Dhttp.port=80
