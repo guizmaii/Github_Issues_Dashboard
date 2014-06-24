c3a3f64eadc8b22ebd0f5a6fb07c1412a6c4604c
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-24 02:34:28 +0200

    Correction de l'emplacement de la sauvegarde des logs

--- launch.sh
+++ launch.sh
@@ -3,6 +3,7 @@
 cat /var/run/redis.pid | kill
 redis-server conf/redis/redis.prod.conf &
 
+mkdir ../loglog/"$(date +'%d-%m-%Y')"/
 sudo cp github_issues_dashboard-1.0-SNAPSHOT/logs/* ../loglog/"$(date +'%d-%m-%Y')"/
 
 sudo rm -rf github_issues_dashboard-1.0-SNAPSHOT/
