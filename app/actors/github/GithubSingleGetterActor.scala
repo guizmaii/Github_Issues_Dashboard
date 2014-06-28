c6d21585c688f9eec3940295893c04fed88733e5
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-28 12:27:47 +0200

    Suppression de code devenu obsolete

--- app/actors/github/GithubSingleGetterActor.scala
+++ app/actors/github/GithubSingleGetterActor.scala
@@ -16,8 +16,6 @@ class GithubSingleGetterActor extends AbstractGithubActor {
   implicit val system = Akka.system
   import system.dispatcher // execution context for futures
 
-  private var theSender: ActorRef = null
-
   val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
 
   private val selfId: Int = self.path.name.split("_")(1).toInt
@@ -27,13 +25,9 @@ class GithubSingleGetterActor extends AbstractGithubActor {
     case link: String =>
       log.debug(s"Next link : ${getPageIndexFromLink(link)}")
 
-      // Je suis obligé de faire une sauvegarde du sender ici, sinon je perds sa référence
-      // et ne peut donc plus l'utiliser dans la méthode handleOkResponse
-      theSender = sender()
-
       pipeline(Get(link)) onComplete {
         case Success(response) =>
-          theSender ! (selfId -> convertResponseToJsObjectList(response))
+          sender ! (selfId -> convertResponseToJsObjectList(response))
 
         case Failure(error) =>
           handleFailureResponse(error)
