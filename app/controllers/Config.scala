0eb7fa283878d105ab0fc3be00d8d6ce2a6c988a
Jules Ivanic <jules.ivanic@gmail.com>
2014-06-30 00:26:08 +0200

    Implémentation de la sécurité (OAuth ne fonctionne pas encore)

--- app/controllers/Config.scala
+++ app/controllers/Config.scala
@@ -1,19 +1,26 @@
 package controllers
 
+import javax.inject.Inject
+
 import actors.DeleteOrder
-import models.GithubRepositoryDAO
+import com.mohiva.play.silhouette.contrib.services.CachedCookieAuthenticator
+import com.mohiva.play.silhouette.core.{Environment, Silhouette}
+import helpers.GithubRepositoryUrlService
+import models.{GithubRepositoryDAO, User}
 import play.api.data.Forms._
 import play.api.data._
 import play.api.data.validation.Constraints._
 import play.api.db.slick._
-import play.api.mvc._
 import redis.RedisActorSingleton
-import services.{GithubRepositoryUrlService, TheGreatDispatcherSingleton}
+import services.TheGreatDispatcherSingleton
 import views.html
+
+import scala.concurrent.Future
 
 case class UserRepoUrl(url: String)
 
-object Config extends Controller {
+class Config @Inject() (implicit val env: Environment[User, CachedCookieAuthenticator])
+  extends Silhouette[User, CachedCookieAuthenticator] {
 
   import play.api.Play.current
 
@@ -32,30 +39,32 @@ object Config extends Controller {
 
   implicit val implicitRepoForm = repoForm
 
-  def create = DBAction {
-    implicit rs =>
-      Ok(html.configuration(GithubRepositoryDAO.getAll))
+  def create = SecuredAction.async { implicit request =>
+    DB.withSession( implicit session =>
+      Future.successful(Ok(html.configuration(GithubRepositoryDAO.getAll, request.identity)))
+    )
   }
 
-  def save = DBAction {
-    implicit rs =>
-      repoForm.bindFromRequest.fold(
-        formWithErrors => Redirect(routes.Config.create()).flashing("failure" -> formWithErrors.errors(0).message),
-        repoUrl => {
-          val githubRepo = GithubRepositoryUrlService.parseUrl(repoUrl.url)
-          GithubRepositoryDAO.insert(githubRepo)
-          TheGreatDispatcherSingleton.instance ! githubRepo
-          Redirect(routes.Config.create()).flashing("success" -> "Dépôt ajouté avec succés")
-        }
-      )
+  def save = SecuredAction.async { implicit request =>
+    repoForm.bindFromRequest.fold(
+      formWithErrors =>
+        Future.successful(Redirect(routes.Config.create()).flashing("failure" -> formWithErrors.errors(0).message)),
+      repoUrl => DB.withSession( implicit session => {
+        val githubRepo = GithubRepositoryUrlService.parseUrl(repoUrl.url)
+        GithubRepositoryDAO.insert(githubRepo)
+        TheGreatDispatcherSingleton.instance ! githubRepo
+        Future.successful(Redirect(routes.Config.create()).flashing("success" -> "Dépôt ajouté avec succés"))
+      })
+    )
   }
 
-  def delete(id: Long) = DBAction {
-    implicit rs =>
+  def delete(id: Long) = SecuredAction.async { implicit request =>
+    DB.withSession( implicit session => {
       // Attention l'ordre des opérations est imporant ici
       val repo = GithubRepositoryDAO.get(id)
       GithubRepositoryDAO.delete(id)
       RedisActorSingleton.instance ! DeleteOrder( repo )
-      Redirect(routes.Config.create()).flashing("success" -> "Dépôt supprimé avec succées")
+      Future.successful(Redirect(routes.Config.create()).flashing("success" -> "Dépôt supprimé avec succées"))
+    })
   }
 }
