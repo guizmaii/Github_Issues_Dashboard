d13da346702e8bd84a9d43a2feee8e717a6d07f0
Jules Ivanic <jules.ivanic@gmail.com>
2015-04-03 19:23:32 +0200

    Esthetique

--- app/controllers/G4Controller.scala
+++ app/controllers/G4Controller.scala
@@ -17,27 +17,21 @@ object G4Controller extends Controller {
 
   import G4JsonProtocol._
 
-  def getAll = DBAction {
-    implicit rs =>
-      val data: List[G4Json] = getFormatedForJs( G4Redis getAll GithubRepositoryDAO.getAllFetched )
-      Ok(Json.parse(data.toJson.compactPrint))
+  def getAll = DBAction { implicit rs =>
+    val data: Iterable[G4Json] = getFormatedForJs(G4Redis getAll GithubRepositoryDAO.getAllFetched)
+    Ok(Json.parse(data.toJson.compactPrint))
   }
 
-  private def getFormatedForJs(mapList: List[Map[String, Int]]): List[G4Json] = {
-    (aggregateStatesAndOccurrences(mapList) map {
-      tuple =>
-        G4Json(tuple._1, tuple._2)
-    }).toList
+  private def getFormatedForJs(mapList: List[Map[String, Int]]): Iterable[G4Json] = {
+    aggregateStatesAndOccurrences(mapList) map { tuple => G4Json(tuple._1, tuple._2) }
   }
 
   private def aggregateStatesAndOccurrences(mapList: List[Map[String, Int]]): Map[String, Int] = {
     var res = Map[String, Int]()
-    mapList map {
-      states =>
-        states map {
-          tuple =>
-            res = res + (tuple._1 -> (res.getOrElse(tuple._1, 0) + tuple._2.asInstanceOf[Int]))
-        }
+    mapList foreach { states =>
+      states foreach { tuple =>
+        res = res + (tuple._1 -> (res.getOrElse(tuple._1, 0) + tuple._2.asInstanceOf[Int]))
+      }
     }
     res
   }
